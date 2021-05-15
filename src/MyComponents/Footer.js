a48b782c9def533a00555674b75d2d2a488e093b
Abhiraj Chatterjee <64791997+Abhiraj3112000@users.noreply.github.com>
2021-05-15 18:22:30 +0530

    Update Footer.js

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Footer = () => {
     return (
-        <footer className="bg-info text-dark py-3">
+        <footer className="bg-success text-dark py-3">
             <p className="text-center" >
            <strong>Copyright &copy; Abhiraj Chatterjee</strong> 
             </p>
