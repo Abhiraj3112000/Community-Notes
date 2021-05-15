9bedee06104f12b49c86fa6d947e6b18b6c297a9
Abhiraj Chatterjee <64791997+Abhiraj3112000@users.noreply.github.com>
2021-05-15 22:53:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ function App() {
   return ( 
     <div className="bg-dark text-light"> 
     <Router>
-      <Header title="My Notes" searchBar={false} /> 
+      <Header title="📑 My Notes" searchBar={false} /> 
       <Switch>
           <Route exact path="/" render={()=>{
             return(
