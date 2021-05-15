1123cc6e13e95e7d3fb5b4aacfc9ab1456184e54
Abhiraj Chatterjee <64791997+Abhiraj3112000@users.noreply.github.com>
2021-05-15 18:20:10 +0530

    Update About.js

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -5,8 +5,8 @@ export const About = () => {
         <div style={{   minHeight: "90vh",
                         margin: "1rem 0 0 1rem ",}}>
             <h3 style={{color:"pink"}}><i>About Community Notes</i></h3> 
-            <p style={{color:"beige"}}><i>This is a web-app made with React.js and Bootstrap and is for everyone to post their notes.
-                <br/>This by no means is a secure note posting website as anyone can add or delete any note.<br/>The Login and Signup feature is to be added later.</i></p>
+            <p style={{color:"beige"}}><i>This is a web-app made with React.js and Bootstrap and is for everyone to post their personal notes.
+                <br/>This is only for personal use and stores information in your local device, no database is involved in this app.<br/>The Login and Signup feature is to be added later.</i></p>
         </div>
     )
 }
